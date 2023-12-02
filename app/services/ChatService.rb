0b9c7eadd2ddc80562eac2a92da771155e5490df
root <mohamed.hany556@gmail.com>
2023-12-02 20:21:29 +0200

    add decrement function

--- app/services/ChatService.rb
+++ app/services/ChatService.rb
@@ -15,13 +15,20 @@ class ChatService
     end
 
     def create_new_chat(app,name)
+        chat = nil
         chat_counts = incr_count(app)
-        last_chat_number = @chat_repository.last_chat_number(app)
-        chat = @chat_repository.create_new_chat(app,name,last_chat_number+1)
-        chat.save
+
+        ActiveRecord::Base.transaction do
+          last_chat_number = @chat_repository.last_chat_number(app)
+          chat = @chat_repository.create_new_chat(app, name, last_chat_number + 1)
+          chat.save
+        end
+
         chat
     end
 
+
+    
     def update_chat(app,params)
         chat = @chat_repository.get_chat_by_number(app,params[:chat_number])
         chat.chat_name = params[:name]
@@ -31,6 +38,7 @@ class ChatService
 
 
     def delete_chat(app,chat_number)
+        chat_count = decr_count(app)
         chat = @chat_repository.get_chat_by_number(app,chat_number)
         chat.destroy
         return chat
@@ -41,7 +49,7 @@ class ChatService
         $redis.watch(app.token) do
             if $redis.get(app.token).nil?
                 $redis.multi do
-                    $redis.sadd("chat_counts", app.token)
+                    $redis.sadd("chat_count", app.token)
                     $redis.set(app.token, app.chat_count)
                 end
             end
@@ -51,4 +59,21 @@ class ChatService
         chat_count
     end
 
+    def decr_count(app)
+        chat_count = 0
+      
+        $redis.watch(app.token) do
+          if $redis.get(app.token).nil?
+            $redis.multi do
+              $redis.sadd("chat_count", app.token)
+              $redis.set(app.token, app.chatCount)
+            end
+          end
+      
+          chat_count = $redis.decr(app.token)
+        end
+      
+        $redis.unwatch
+        chat_count
+    end
 end
